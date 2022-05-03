942dc131c46ead98ab2a44610a4812aedf443a3a
Gurshobit Brar <100031605+gbrar1992@users.noreply.github.com>
2022-05-03 12:20:59 +0530

    Q1 Commit
    Q1 Commit

---
+++ src/com/gurshobit/questionone/BuildingProblem.java
@@ -0,0 +1,48 @@
+package com.gurshobit.questionone;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BuildingProblem {
+    public static Scanner scannerInput = new Scanner(System.in);
+    public static void floorWorkLogic(int[] floors, int maxFloors){
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(java.util.Collections.reverseOrder());
+
+        int maxFloorNumber = maxFloors;
+
+        for (int f = 0; f < maxFloors; f++) {
+            System.out.println("Day " + (f+1) +": ");
+
+            priorityQueue.add(floors[f]);
+
+            while(!priorityQueue.isEmpty() && priorityQueue.peek() == maxFloorNumber){
+                System.out.print(priorityQueue.poll() + " ");
+                maxFloorNumber--;
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        int totalFloors = 0;
+        do {
+            System.out.println("Enter Number of floors in building");
+            totalFloors = Integer.parseInt(scannerInput.nextLine());
+        } while (totalFloors < 1);
+
+        int[] floors = new int[totalFloors];
+
+        for (int f = 0; f < totalFloors; f++){
+            int floorSize = 0;
+            do {
+                System.out.println("Enter Floor Size on Day " + (f+1) + ":");
+                floorSize = Integer.parseInt(scannerInput.nextLine());
+            } while (floorSize < 1);
+            floors[f] = floorSize;
+        }
+        System.out.println("Building Floor Construction Plan as follows: ");
+        floorWorkLogic(floors,totalFloors);
+    }
+}
